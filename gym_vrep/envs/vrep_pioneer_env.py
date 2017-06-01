766197709a9b3c086f1192e9cf80506b06670dfc
Brian Yang <brianhyang@berkeley.edu>
2017-05-31 17:37:52 -0700

    Works with the Walker, though the feet aren't finished. Also re-organized files.

--- gym_vrep/envs/vrep_pioneer_env.py
+++ gym_vrep/envs/vrep_pioneer_env.py
@@ -1,22 +1,14 @@
 from gym_vrep.envs.vrep_env import VrepEnv
 
 class VrepPioneerEnv(VrepEnv):
-	metadata = {'render.modes': ['human']}
 
 	def __init__(self):
 		super().__init__('pioneer.ttt', ['pioneer', 'leftMotor', 'rightMotor'])
 
 	def _step(self, action):
+		# action is a list/tuple of size 2, which corresponds to the velocities of left and right motors
 		self.setJointTargetVelocity('leftMotor', action[0])
 		self.setJointTargetVelocity('rightMotor', action[1])
 		VrepEnv._step(self, action)
 		ob = self.getObjectPosition('pioneer'), self.getObjectVelocity('pioneer')
 		return ob, 0, False, {}
-
-	# def _reset(self):
-	# 	print('_reset called from pioneer class')
-	# 	# VrepEnv._reset(self)
-
-	# def _render(self, mode='human', close=False):
-	# 	print('_render called from pioneer class')
-	# 	# VrepEnv._render(self, mode, close)
--- gym_vrep/envs/vrep_walker_env.py
+++ gym_vrep/envs/vrep_walker_env.py
@@ -1,19 +1,19 @@
-import gym
-from gym import error, spaces, utils
-from gym.utils import seeding
+from gym_vrep.envs.vrep_env import VrepEnv
 
-class VrepWalkerEnv(gym.Env):
-	metadata = {'render.modes': ['human']}
+class VrepWalkerEnv(VrepEnv):
 
 	def __init__(self):
-		print('__init__')
+		super().__init__('walker.ttt', ['walker', 'leg1', 'leg2', 'leg3', 'leg4', 'leg5', 'leg6', 'foot1', 'foot2', 'foot3', 'foot4', 'foot5', 'foot6'])
 
 	def _step(self, action):
-		print('_step')
-		return None
-
-	def _reset(self):
-		print('_reset')
-
-	def _render(self, mode='human', close=False):
-		print('_render')
+		# action is a list/tupe of size 6, which corresponds to the velocities of the 6 legs
+		print(action)
+		self.setJointTargetVelocity('leg1', action[0])
+		self.setJointTargetVelocity('leg2', action[1])
+		self.setJointTargetVelocity('leg3', action[2])
+		self.setJointTargetVelocity('leg4', action[3])
+		self.setJointTargetVelocity('leg5', action[4])
+		self.setJointTargetVelocity('leg6', action[5])
+		VrepEnv._step(self, action)
+		ob = self.getObjectPosition('walker'), self.getObjectVelocity('walker')
+		return ob, 0, False, {}
